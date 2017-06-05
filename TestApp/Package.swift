2ac172b2fac1f0bf64bd567a59b0a8d88198d6e8
Hannes Sverrisson <sverrisson@ruv.is>
2017-06-05 23:53:50 +0000

    Updated package

--- TestApp/Package.swift
+++ TestApp/Package.swift
@@ -3,5 +3,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestApp"
+    name: "TestApp",
+    dependencies: [
+        .Package(url: "../TesseractSwift", majorVersion: 1)
+    ]
 )
+
+// https://github.com/sverrisson/TesseractSwift.git
