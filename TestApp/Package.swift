3bc367db0474530d6e7b0e5a95854eabd62de642
Hannes Sverrisson <hannessv@simnet.is>
2017-06-06 20:48:11 +0000

    Update Package.swift
    Use majorversion 0

--- TestApp/Package.swift
+++ TestApp/Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "TestApp",
     dependencies: [
-        .Package(url: "https://github.com/sverrisson/TesseractSwift.git", majorVersion: 1)
+        .Package(url: "https://github.com/sverrisson/TesseractSwift.git", majorVersion: 0)
     ]
 )
